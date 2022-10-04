7bfe8e8e0734323d4c6cb47c37e9473e2f1e6b19
memsharded <james@conan.io>
2022-10-04 10:23:10 +0200

    generate json

--- tests/recipes/pkg_b/conanfile.py
+++ tests/recipes/pkg_b/conanfile.py
@@ -1,5 +1,6 @@
 from conans import ConanFile
-import os
+from conan.tools.files import save, copy
+import os, json
 
 class pkg_b(ConanFile):
   name = "pkg_b"
@@ -11,6 +12,21 @@ class pkg_b(ConanFile):
     "pkg_a/1.0"
   )
 
+  def generate(self):
+    requires = []
+    tool_requires = []
+    for d in self.dependencies.values():
+      if d.context == "build":
+        tool_requires.append(repr(d.ref))
+      else:
+        requires.append(repr(d.ref))
+    result = {"requires": requires,
+              "tool_requires": tool_requires}
+    save(self, "msdeps.json", json.dumps(result))
+
+  def package(self):
+    copy(self, "msdeps.json", dst=self.package_folder, src=self.generators_folder)
+    
   def package_id(self):
     self.info.header_only()
 
